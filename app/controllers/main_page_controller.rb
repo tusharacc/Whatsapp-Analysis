e71376177db9ca7101bea74f7ebcc72ac165fb49
Tushar Saurabh <tusharacc@gmail.com>
2016-07-25 00:26:11 +0530

    Morris Chart working

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -90,9 +90,23 @@ class MainPageController < ApplicationController
 	end
 
 	def update_message_list
-    	@messages = []
+    	get_count_per_member
+		logger.debug "The message summary is #{@messages}"
+		get_average_per_day
+    	get_media_omitted
+    	get_members_added
+    	respond_to do |format|
+      		format.js 
+      		format.html
+    	end
+  	end
 
-    	file = File.read('public', 'uploads', session[:file_name]+'_regular.json')
+  	#private
+
+  	def get_count_per_member
+  		@messages = []
+    	#logger.debug "the file name is #{{}}"
+    	file = File.read(Rails.root.join('public', 'uploads', session[:file_name]+'_regular.json'))
     	text_hash = JSON.parse(file)
 
 		text_hash.each do |hsh|
@@ -112,28 +126,60 @@ class MainPageController < ApplicationController
 				@messages.push({'name'=>name,'count'=>1})
 			end
 		end
-		#file = File.new(Rails.root.join('public', 'uploads', session[:file_name]+"_regular.txt"), "r")
-  		
-		file.close
-		logger.debug "The message summary is #{@messages}"
-    	get_media_omitted
-    	get_members_added
-    	respond_to do |format|
-      		format.js 
-      		format.html
-    	end
+
   	end
 
-  	#private
+  	def get_average_per_day
+  		dt = ''
+  		@messages_per_day = []
+  		day_map = {"1"=>"Monday","2"=>"Tuesday","3"=>"Wednesday","4"=>"Thursday","5"=>"Friday","6"=>"Saturday","7"=>"Sunday"}
+  		file = File.read(Rails.root.join('public', 'uploads', session[:file_name]+'_regular.json'))
+    	text_hash = JSON.parse(file)
+    	latest_date = Date.strptime(text_hash[0]['date'],'%m/%d/%Y')
+    	text_hash.each do |hsh|
+    		logger.debug "the hsh is #{hsh}"
+			dt = hsh['date']
+			day_num = Date.strptime(dt, '%m/%d/%Y').cwday 
+			not_found = true
+			a = 0
+			@messages_per_day.each do |rec|
+				if rec['day'] == day_map[day_num.to_s]
+					not_found = false
+					@messages_per_day[a]['count'] = rec['count'] + 1
+				end
+				a += 1
+			end
+			if not_found
+				@messages_per_day.push({'day'=>day_map[day_num.to_s],'count'=>1})
+			end
+			
+    	end
+    	day_count = {"1"=>0,"2"=>0,"3"=>0,"4"=>0,"5"=>0,"6"=>0,"7"=>0}
+    	(Date.strptime(dt,'%m/%d/%Y')..latest_date).each do |orig_date|
+    		day_count.each do |k,v|
+				if k == orig_date.cwday.to_s
+					day_count[k] = v + 1
+				end
+    		end
+    	end
+
+    	logger.debug "the number of messages per day is #{day_count}"
+    	logger.debug "the number of messages per day is #{@messages_per_day}"
+    	
+    	@messages_per_day.each_with_index do |val,index| 
+    		@messages_per_day[index]['count'] = @messages_per_day[index]['count']/day_count[@messages_per_day[index]['day']]  
+    	end
+
+  	end
 
   	def get_media_omitted
   		count = 0
   		file = File.new(Rails.root.join('public', 'uploads', session[:file_name]+"_specific.txt"), "r")
-  		logger.debug "File Name is #{Rails.root.join('public', 'uploads', session[:file_name]+"_specific.txt")}"
+  		#logger.debug "File Name is #{Rails.root.join('public', 'uploads', session[:file_name]+"_specific.txt")}"
   		file.each do |line|
   			line_split = line.scan(/(\d+\/\d+\/\d+),\s(\d+:\d+\s\w+)\s-\s(.*$)/)
 		    if line_split[0][-1] == "<Media omitted>"
-				logger.debug 'I am in, what you want to do'
+				#logger.debug 'I am in, what you want to do'
 				count += 1
     		end
 
@@ -153,19 +199,19 @@ class MainPageController < ApplicationController
 	  	# end
 
   		@media_added = count
-  		logger.debug "The number of time media was added #{@media_added}"
+  		#logger.debug "The number of time media was added #{@media_added}"
   	end
 
   	def get_members_added
   		count = 0
   		file = File.new(Rails.root.join('public', 'uploads', session[:file_name]+"_specific.txt"), "r")
-  		logger.debug "File Name is #{Rails.root.join('public', 'uploads', session[:file_name]+"_specific.txt")}"
+  		#logger.debug "File Name is #{Rails.root.join('public', 'uploads', session[:file_name]+"_specific.txt")}"
   		file.each do |line|
   			line_split = line.scan(/(\d+\/\d+\/\d+),\s(\d+:\d+\s\w+)\s-\s(.*$)/)
   			#if line_split.count != 0 
   				if line_split[0].count == 3
-  					logger.debug "The line we are looking at #{line_split[0][-1]}"
-  					logger.debug "The index we are looking at #{line_split[0][-1].index("added")}"
+  					#logger.debug "The line we are looking at #{line_split[0][-1]}"
+  					#logger.debug "The index we are looking at #{line_split[0][-1].index("added")}"
   					if line_split[0][-1].index("added") != nil
 						count += 1
 					end
